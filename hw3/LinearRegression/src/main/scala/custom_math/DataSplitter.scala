d590450dc30c2be74687ea3327f8698712e77553
Azamat Sabyrbayev <azamat.sabyrbayev@btsdigital.kz>
2021-10-23 01:27:15 +0600

    Remove repeated blocks

--- hw3/LinearRegression/src/main/scala/custom_math/DataSplitter.scala
+++ hw3/LinearRegression/src/main/scala/custom_math/DataSplitter.scala
@@ -7,11 +7,12 @@ class DataSplitter{
   def split(dataMatrix: DenseMatrix[Double], valSize: Int) = {
       val target = dataMatrix(::, -1)
       val xData = dataMatrix(::, Range(0, dataMatrix.cols - 1))
-
-      val xTrain = xData(0 to xData.rows - valSize, ::)
-      val xVal = xData(xData.rows - valSize to xData.rows - 1, ::)
-      val yTrain = target(0 to xData.rows - valSize)
-      val yVal = target(xData.rows - valSize to xData.rows - 1)
+      val rows = xData.rows
+      val trainSize = rows - valSize
+      val xTrain = xData(0 to trainSize, ::)
+      val xVal = xData(trainSize to rows - 1, ::)
+      val yTrain = target(0 to trainSize)
+      val yVal = target(trainSize to rows - 1)
 
       (xTrain, yTrain, xVal, yVal)
     }
