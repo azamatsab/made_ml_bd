69f45c65949d9b838d632d7761f0989a9eca3d41
Azamat Sabyrbayev <azamat.sabyrbayev@btsdigital.kz>
2021-10-23 01:32:02 +0600

    Rename value

--- hw3/LinearRegression/src/main/scala/custom_math/LinRegression.scala
+++ hw3/LinearRegression/src/main/scala/custom_math/LinRegression.scala
@@ -3,13 +3,13 @@ package custom_math
 import breeze.linalg._
 
 class LinRegression {
-  var weights = Vector[Double](1, 1)
+  var weights = Vector[Double](0)
 
   def fit(xMatrix: DenseMatrix[Double], yVector: Vector[Double]) = {
     val dotXx = xMatrix.t * xMatrix
     val dotXy = xMatrix.t * yVector
-    val part1 = pinv(dotXx)
-    this.weights = part1 * dotXy
+    val invDotXx = pinv(dotXx)
+    this.weights = invDotXx * dotXy
   }
 
   def predict(xMatrix: DenseMatrix[Double]) = {
